8030fe7b7845133895ad427c49a7892aefe428e5
Jack Tseng <jacktseng@jorjin.com.tw>
2021-09-08 16:03:59 +0800

    First commit

---
+++ .theme/config.js
@@ -0,0 +1,79 @@
+//
+/*------------------------------------*\
+    Originally used in Nginxy by @lfelipe1501
+
+    Modified by @jacktseng831
+\*------------------------------------*/
+// Configure .theme here:
+//
+//
+
+$(document).ready(function(){
+	// Override the headline with breadcrumbs
+	//var loc = $("h1").text();
+	var loc = window.location.pathname;
+	var segments = loc.split('/');
+	var breadcrumbs = '';
+	var currentPath = '/';
+	for (var i = 0; i < segments.length; i++) {
+		if (segments[i] !== '') {
+			currentPath += segments[i] + '/';
+			breadcrumbs += '/<a href="' + currentPath + '" class=\"breadcrumbs\">' + window.unescape(segments[i]) + '<\/a>';
+		} else if (segments.length - 1 !== i) {
+			currentPath += '';
+			breadcrumbs += '<a href="' + currentPath + '" class=\"breadcrumbs\">File System<\/a>';
+		}
+	}
+	$("h1").html(breadcrumbs);
+
+	// Establish supported formats.
+	var list = new Array();
+	var formats = ["bin", "jpg", "gif", "bmp", "png", "html", "css", "zip", "iso", "tiff", "ico", "psd", "pdf", "exe", "rar", "deb", "swf", "7z", "doc", "docx", "xls", "xlsx", "pptx", "ppt", "txt", "php", "js", "c", "c++", "torrent", "sql", "wmv", "avi", "mp4", "mp3", "wma", "ogg", "msg", "wav", "py", "java", "gzip", "jpeg", "raw", "cmd", "bat", "sh", "svg"];
+	
+	// Scan all files in the directory, check the extensions and show the right MIME-type image.
+	$('td a').each(function(){
+		var found = 0;
+		var arraySplit = $(this).attr('href').split(".");
+		var fileExt = arraySplit[arraySplit.length - 1];
+	
+		for (var i = 0; i < formats.length; i++) {
+			if (fileExt.toLowerCase() == formats[i].toLowerCase()) {
+				var found = 1;
+				var oldText = $(this).text();
+				$(this).html('<img class="icons" src="/.theme/icons/' + formats[i] + '.png" style="margin:0px 4px -4px 0px"></img></a>' + oldText);
+				return;
+			}
+		}
+
+		// Add an icon for the go-back link.
+		if ($(this).text().indexOf("Parent directory") >= 0) {
+			var found = 1;
+			var oldText = $(this).text();
+			$(this).html('<img class="icons" src="/.theme/icons/home.png" style="margin:0px 4px -4px 0px">' + oldText);
+			return;
+		}
+	
+
+		// Check for folders as they don't have extensions.
+		if ($(this).attr('href').substr($(this).attr('href').length - 1) == '/') {
+			var found = 1;
+			var oldText = $(this).text();
+			$(this).html('<img class="icons" src="/.theme/icons/folder.png" style="margin:0px 4px -4px 0px">' + oldText.substring(0, oldText.length - 1));
+	
+			// Fix for annoying jQuery behaviour where inserted spaces are treated as new elements -- which breaks my search.
+			var string = ' ' + $($(this)[0].nextSibling).text();
+	
+			// Copy the original meta-data string, append a space char and save it over the old string.
+			$($(this)[0].nextSibling).remove();
+			$(this).after(string);
+			return;
+		}
+
+		// File format not supported by Better Listings, so let's load a generic icon.
+		if (found == 0){
+			var oldText = $(this).text();
+			$(this).html('<img class="icons" src="/.theme/icons/error.png" style="margin:0px 4px -4px 0px">' + oldText);
+			return;
+		}
+	});
+});
---
+++ README.md
@@ -0,0 +1,38 @@
+# Nginx-Fancyindex-Theme
+A nginx fancyindex theme based on [Nginx-Fancyindex-Theme @Naereen](https://github.com/Naereen/Nginx-Fancyindex-Theme).
+Comes with updated [JQuery](https://jquery.com/) and [ShowDown.js](https://github.com/showdownjs/showdown/), a dark mode toggle, and file icons from ([Nginxy-Theme @ifelipe1501](https://github.com/lfelipe1501/Nginxy/tree/master/Nginxy-Theme/images/icons)).
+
+The fancyindex module can be found [here](https://github.com/aperezdc/ngx-fancyindex) (by @aperezdc).
+The fancyindex docker can be found [here](https://github.com/byjg/docker-nginx-extras) (by @byjg).
+
+[![made-for-nginx](https://img.shields.io/badge/Made%20for-nginx-1f425f.svg)](https://www.nginx.org/)
+
+
+### Screenshots:
+![light-theme](screenshots/light_theme_example.png)
+![dark-theme](screenshots/dark_theme_example.png)
+
+## Usage
+
+1. Make sure you have the fancyindex module compiled with nginx, either by compiling it yourself or installing nginx via the full distribution (paquet `nginx-extras`).
+2. Include the content of [fancyindex.conf](fancyindex.conf) in your location directive (`location / {.....}`) in your nginx config (usually `nginx.conf`).
+3. Move the `.theme/` folder to the root of the site directory.
+4. Restart/reload nginx.
+5. Check that it's working, and enjoy!
+
+## Configuration
+
+A standard config looks something like this:
+
+```bash
+fancyindex on;
+fancyindex_localtime on;
+fancyindex_exact_size off;
+# Specify the path to the header.html and foother.html files, that are server-wise,
+fancyindex_header "/.theme/header.html";
+fancyindex_footer "/.theme/footer.html";
+# Making sure folder where these files are do not show up in the listing.
+fancyindex_ignore ".theme";
+fancyindex_name_length 255;
+```
+
